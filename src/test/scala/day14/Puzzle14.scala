2da6bbbd6d6ce8f42bdf2caf6836278d82e564de
Crosson David <crosson.david@gmail.com>
2023-12-22 13:51:25 +0100

    Solve Day14 star1

--- src/test/scala/day14/Puzzle14.scala
+++ src/test/scala/day14/Puzzle14.scala
@@ -4,7 +4,21 @@ import zio.*
 import zio.test.*
 import zio.test.TestAspect.*
 
-case class Coord(x: Int, y: Int)
+opaque type Coord = Int
+
+extension (c: Coord) {
+  def x      = c % Coord.width
+  def y      = c / Coord.width
+  def right  = c + 1
+  def left   = c - 1
+  def north  = c - Coord.width
+  def south  = c + Coord.width
+  def around = List(c.south, c.right, c.north, c.left)
+}
+object Coord {
+  val width                        = 10000
+  def apply(x: Int, y: Int): Coord = y * Coord.width + x
+}
 
 case class Platform(
   rollingRocks: Set[Coord],
@@ -30,21 +44,53 @@ def parse(input: List[String]) = {
 
 // ------------------------------------------------------------------------------
 def slideNorth(rollingRocks: Set[Coord], platform: Platform): Set[Coord] = {
-  val rollingRocksByX     = rollingRocks.groupBy(_.x)
   var updatedRollingRocks = rollingRocks
-  0.to(platform.maxX).map { x =>
-    0.to(platform.maxY).map { y =>
-      val coord = Coord(x, y)
-
+  var currentX            = 0
+  while (currentX <= platform.maxX) {
+    var currentY = 0
+    while (currentY <= platform.maxY) {
+      val current        = Coord(currentX, currentY)
+      def currentIsEmpty = !updatedRollingRocks.contains(current) && !platform.cubicRocks.contains(current)
+      var other          = current.south
+      while (currentIsEmpty && other.y <= platform.maxY && !platform.cubicRocks.contains(other)) {
+        if (updatedRollingRocks.contains(other)) {
+          updatedRollingRocks = updatedRollingRocks - other + current
+        }
+        other = other.south
+      }
+      currentY = currentY + 1
     }
+    currentX = currentX + 1
   }
-  ???
+  updatedRollingRocks
+}
+
+def valueNorth(rollingRocks: Set[Coord], platform: Platform): Int = {
+  0.to(platform.maxY)
+    .map { y =>
+      val count = 0.to(platform.maxX).count(x => rollingRocks.contains(Coord(x, y)))
+      count * (1 + platform.maxY - y)
+    }
+    .sum
+}
+
+def show(rollingRocks: Set[Coord], platform: Platform): Unit = {
+  0.to(platform.maxY).foreach { y =>
+    0.to(platform.maxX).foreach { x =>
+      val coord = Coord(x, y)
+      if (platform.cubicRocks.contains(coord)) print("#")
+      else if (rollingRocks.contains(coord)) print("O")
+      else print(".")
+    }
+    println()
+  }
 }
 
 def resolveStar1(input: List[String]): Int = {
   val platform          = parse(input)
   val movedRollingRocks = slideNorth(platform.rollingRocks, platform)
-  ???
+  //show(movedRollingRocks, platform)
+  valueNorth(movedRollingRocks, platform)
 }
 
 // ------------------------------------------------------------------------------
@@ -67,7 +113,7 @@ object Puzzle14Test extends ZIOSpecDefault {
         puzzleResult  = resolveStar1(puzzleInput)
       } yield assertTrue(
         exampleResult == 136,
-        puzzleResult == 0
+        puzzleResult == 105982
       )
     },
     test("star#2") {
@@ -77,7 +123,7 @@ object Puzzle14Test extends ZIOSpecDefault {
         puzzleInput   <- fileLines(Path(s"data/$day/puzzle-1.txt"))
         puzzleResult   = resolveStar2(puzzleInput)
       } yield assertTrue(
-        exampleResult1 == 0,
+        exampleResult1 == 64,
         puzzleResult == 0
       )
     }
